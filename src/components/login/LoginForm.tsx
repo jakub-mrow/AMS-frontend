94a14dca84005a07781f262f12ded9d323c0d0f2
jakub-mrow <jakub@mrowczynski.net>
2023-07-16 20:16:37 +0200

    login request from client

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,22 +1,54 @@
 import { useState } from 'react';
-
-import { TextField } from "@mui/material"
+import { AlertColor, TextField } from "@mui/material"
 import { Link } from 'react-router-dom';
-import { IconButton, InputAdornment } from '@mui/material';
+import { IconButton, InputAdornment, Snackbar, Alert } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-
 import { FaDollarSign } from 'react-icons/fa';
+import { authenticateLogin } from '../../api/authRequests';
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
 
 
 const LoginForm = () => {
     const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [showAlert, setShowAlert] = useState<string | null>(null);
+    const [alertSeverity, setAlertSeverity] = useState<AlertColor>("error");
+
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
 
     const handleTogglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
 
-    const handleSubmit = () => {
+    let navigate = useNavigate();
+    const changeToMainRoute = () => { 
+        const path = "/"; 
+        navigate(path);
+    }
 
+    const {register, handleSubmit, formState: { errors }} = useForm();
+
+    const onSubmit = async () => {
+        try{
+            const response = await authenticateLogin(username, password);
+
+            if (response.success){
+                setAlertSeverity("success");
+                setShowAlert(response.message as string);
+                setTimeout(() => {
+                    changeToMainRoute();
+                }, 2000);
+
+            } else {
+                setAlertSeverity("error");
+                setShowAlert(response.message as string);
+            }
+
+        } catch(e){
+            console.log(e);
+        }
     }
 
     return (
@@ -30,14 +62,14 @@ const LoginForm = () => {
                 <p className="text-xl mt-4 whitespace-normal">Asset management system is the best place to keep track of all your investments</p>
             </div>
             
-        
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mt-14">
                     <TextField 
                         id="username" 
                         label="Username" 
                         variant="outlined" 
                         className="w-96"
+                        onChange={(event) => setUsername(event.target.value)}
                     />
                 </div>
                 <div className="mt-8">
@@ -46,6 +78,7 @@ const LoginForm = () => {
                         label="Password"
                         type={showPassword ? 'text' : 'password'}
                         className="w-96"
+                        onChange={(event) => setPassword(event.target.value)}
                         InputProps={{
                             endAdornment: (
                             <InputAdornment position="end">
@@ -79,6 +112,14 @@ const LoginForm = () => {
                 </Link>
             </div>
     
+            <Snackbar 
+                anchorOrigin={{vertical: 'bottom', horizontal: 'right'}} 
+                key={'bottom' + 'right'} 
+                open={showAlert !== null} 
+                autoHideDuration={5000} 
+                onClose={() => setShowAlert(null)}>
+                    <Alert severity={alertSeverity}>{showAlert}</Alert>
+            </Snackbar>
         </>
     )
 }
