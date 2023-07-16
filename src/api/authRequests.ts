74a1b00ecccfd47186b811db1ecbf20937011bb6
jakub-mrow <jakub@mrowczynski.net>
2023-07-16 20:29:55 +0200

    sending register request

--- src/api/authRequests.ts
+++ src/api/authRequests.ts
@@ -40,3 +40,42 @@ export async function authenticateLogin(username: string, password: string): Pro
         }
     }
 }
+
+
+export async function registerRequest(username: string, password: string, email: string): Promise<ApiResponse>{
+    try {
+        const response = await fetch("http://localhost:8000/auth/register/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+              },
+            body: JSON.stringify({username, password, email}),
+        });
+
+        if (!response.ok){
+            let responseData: ApiResponse = {success: false, message: null, data: await response.json()};
+            if (response.status === 400) {
+                responseData.message = JSON.stringify(await response.json());
+                return responseData;
+            } else if (response.status === 500){
+                responseData.message = "Internal server error";
+                return responseData;
+            }
+        }
+
+        const data = await response.json();
+    
+        return {
+            success: true,
+            message: "Successfully created the account",
+            data: data
+        }
+
+    } catch(e) {
+        return {
+            success: false,
+            message: "Internal server error",
+            data: null
+        }
+    }
+}
--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -1,21 +1,56 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
-import { TextField } from "@mui/material"
-import { Link } from 'react-router-dom';
-import { IconButton, InputAdornment } from '@mui/material';
+import { AlertColor, TextField } from "@mui/material"
+import { Link, useNavigate } from 'react-router-dom';
+import { IconButton, InputAdornment, Snackbar, Alert } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 import { FaDollarSign } from 'react-icons/fa';
+import { registerRequest } from '../../api/authRequests';
+import { useForm } from 'react-hook-form';
 
 const RegisterForm = () => {
     const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [showAlert, setShowAlert] = useState<string | null>(null);
+    const [alertSeverity, setAlertSeverity] = useState<AlertColor>("error");
+
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [repeatedPassword, setRepeatedPassword] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
 
     const handleTogglePasswordVisibility = () => {
         setShowPassword(!showPassword);
     };
 
-    const handleSubmit = () => {
+    let navigate = useNavigate();
+    const changeToMainRoute = () => { 
+        const path = "/"; 
+        navigate(path);
+    }
 
+    const {register, handleSubmit, formState: { errors }} = useForm();
+
+    const onSubmit = async () => {
+        try{
+            const response = await registerRequest(username, password, email);
+
+            if (response.success){
+                setAlertSeverity("success");
+                setShowAlert(response.message as string);
+                setTimeout(() => {
+                    changeToMainRoute();
+                }, 2000);
+
+            } else {
+                setAlertSeverity("error");
+                setShowAlert(response.message as string);
+            }
+
+        } catch(e){
+            setAlertSeverity("error");
+            setShowAlert("Internal server error");
+        }
     }
     return (
         <>
@@ -24,77 +59,88 @@ const RegisterForm = () => {
             </div>
 
             <h1 className="text-6xl font-semibold mt-20">Create your AMS account</h1>
-            <form onSubmit={handleSubmit}>
-                    <div className="mt-14">
-                        <TextField 
-                            id="username" 
-                            label="Username" 
-                            variant="outlined" 
-                            className="w-96"
-                        />
-                    </div>
-                    <div className="mt-8">
-                        <TextField 
-                            id="email" 
-                            label="Email" 
-                            variant="outlined" 
-                            className="w-96"
-                        />
-                    </div>
-                    <div className="mt-8">
-                        <TextField
-                            id="password"
-                            label="Password"
-                            type={showPassword ? 'text' : 'password'}
-                            className="w-96"
-                            InputProps={{
-                                endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton onClick={handleTogglePasswordVisibility}>
-                                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </div>
-                    <div className="mt-8">
-                        <TextField
-                            id="repeatPassword"
-                            label="Repeat Password"
-                            type={showPassword ? 'text' : 'password'}
-                            className="w-96"
-                            InputProps={{
-                                endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton onClick={handleTogglePasswordVisibility}>
-                                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                                    </IconButton>
-                                </InputAdornment>
-                                ),
-                            }}
-                        />
-                    </div>
-                    <div className="mt-16">
-                        <button 
-                            className="w-full mt-4 h-12 text-xl bg-primary text-white rounded-xl shadow-lg" 
-                            type="submit" >
-                            Register
-                        </button>
-                    </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="mt-14">
+                    <TextField 
+                        id="username" 
+                        label="Username" 
+                        variant="outlined" 
+                        className="w-96"
+                        onChange={(event) => setUsername(event.target.value)}
+                    />
+                </div>
+                <div className="mt-8">
+                    <TextField 
+                        id="email" 
+                        label="Email" 
+                        variant="outlined" 
+                        className="w-96"
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+                <div className="mt-8">
+                    <TextField
+                        id="password"
+                        label="Password"
+                        type={showPassword ? 'text' : 'password'}
+                        className="w-96"
+                        onChange={(event) => setPassword(event.target.value)}
+                        InputProps={{
+                            endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={handleTogglePasswordVisibility}>
+                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                            ),
+                        }}
+                    />
+                </div>
+                <div className="mt-8">
+                    <TextField
+                        id="repeatPassword"
+                        label="Repeat Password"
+                        type={showPassword ? 'text' : 'password'}
+                        onChange={(event) => setRepeatedPassword(event.target.value)}
+                        className="w-96"
+                        InputProps={{
+                            endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={handleTogglePasswordVisibility}>
+                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                            ),
+                        }}
+                    />
+                </div>
+                <div className="mt-16">
+                    <button 
+                        className="w-full mt-4 h-12 text-xl bg-primary text-white rounded-xl shadow-lg" 
+                        type="submit" >
+                        Register
+                    </button>
+                </div>
 
-                    <div className="flex items-center justify-center mt-8">
-                        <div className="border-b-2 w-1/3"></div>
-                        <div className="border-b-2 w-1/3"></div>
-                    </div>
+                <div className="flex items-center justify-center mt-8">
+                    <div className="border-b-2 w-1/3"></div>
+                    <div className="border-b-2 w-1/3"></div>
+                </div>
 
-                    <div className="flex justify-center items-center mt-4">
-                        <Link to="/login" className="text-primary ml-4">
-                            Back
-                        </Link>
-                    </div>
-                
-                </form>
+                <div className="flex justify-center items-center mt-4">
+                    <Link to="/login" className="text-primary ml-4">
+                        Back
+                    </Link>
+                </div>
+            </form>
+            <Snackbar 
+                anchorOrigin={{vertical: 'bottom', horizontal: 'right'}} 
+                key={'bottom' + 'right'} 
+                open={showAlert !== null} 
+                autoHideDuration={5000} 
+                onClose={() => setShowAlert(null)}>
+                    <Alert severity={alertSeverity}>{showAlert}</Alert>
+            </Snackbar>                
         </>
     )
 }
