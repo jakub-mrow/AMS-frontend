e21fbd4bae726653a76ff0657649d89a73273414
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-08-19 20:05:53 +0200

    feat: Add dialog when deleting an account

--- src/accounts/AccountsTable.tsx
+++ src/accounts/AccountsTable.tsx
@@ -10,6 +10,8 @@ import {
 } from "@mui/material";
 import { Delete, Edit } from "@mui/icons-material";
 import { Account } from "./accounts-service.ts";
+import { useState } from "react";
+import { ConfirmationDialog } from "../dialog/ConfirmationDialog.tsx";
 
 export const AccountsTable = ({
   accounts,
@@ -20,33 +22,54 @@ export const AccountsTable = ({
   onAccountDelete: (accountId: number) => void;
   onAccountUpdate: (account: Account) => void;
 }) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [accountToDelete, setAccountToDelete] = useState<Account | null>(null);
+  const onDeleteConfirm = () => {
+    if (accountToDelete) {
+      onAccountDelete(accountToDelete.id);
+    }
+    setIsDialogOpen(false);
+  };
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Account</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Actions</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {accounts.map((account) => (
-            <TableRow key={account.id} hover onClick={() => {}}>
-              <TableCell>{account.id}</TableCell>
-              <TableCell>{account.name}</TableCell>
-              <TableCell>
-                <IconButton onClick={() => onAccountDelete(account.id)}>
-                  <Delete />
-                </IconButton>
-                <IconButton onClick={() => onAccountUpdate(account)}>
-                  <Edit />
-                </IconButton>
-              </TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Account</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Actions</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {accounts.map((account) => (
+              <TableRow key={account.id} hover onClick={() => {}}>
+                <TableCell>{account.id}</TableCell>
+                <TableCell>{account.name}</TableCell>
+                <TableCell>
+                  <IconButton
+                    onClick={() => {
+                      setAccountToDelete(account);
+                      setIsDialogOpen(true);
+                    }}
+                  >
+                    <Delete />
+                  </IconButton>
+                  <IconButton onClick={() => onAccountUpdate(account)}>
+                    <Edit />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <ConfirmationDialog
+        isOpen={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+        onConfirm={onDeleteConfirm}
+        content={`Are you sure you want to delete account ${accountToDelete?.name}?`}
+      />
+    </>
   );
 };
