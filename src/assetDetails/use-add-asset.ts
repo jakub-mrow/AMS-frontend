37ed853d9314520b00b9ddda4e9689f37e1d6fc5
Jakub Mrówczyński <jakub@mrowczynski.net>
2023-11-19 22:48:37 +0100

    adding asset from assetDetails window

---
+++ src/assetDetails/use-add-asset.ts
@@ -0,0 +1,98 @@
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import AuthContext from "../auth/auth-context";
+import { AccountsDetailsService } from "../accountDetails/account-details-service";
+import { apiUrl } from "../config";
+import { useSnackbar } from "../snackbar/use-snackbar";
+import { Dayjs } from "dayjs";
+import { Severity } from "../snackbar/snackbar-context";
+import { Account } from "../types";
+import { AccountsService } from "../accounts/accounts-service";
+
+
+export enum DialogType {
+    TRANSACTION,
+    STOCK,
+}
+
+export const useAddAsset = () => {
+    const { token } = useContext(AuthContext);
+    const accountDetailsService = useMemo(() => {
+        return new AccountsDetailsService(apiUrl, token);
+    }, [token]);
+
+    const accountsService = useMemo(() => {
+        return new AccountsService(apiUrl, token);
+    }, [token]);
+
+    const alert = useSnackbar();
+
+    const [dialogType, setDialogType] = useState<DialogType>(
+        DialogType.TRANSACTION,
+    );
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [accounts, setAccounts] = useState<Account[]>([]);
+
+
+    useEffect(() => {
+        accountsService
+            .fetchAccounts()
+            .then((data) => {
+                setAccounts(data);
+            })
+            .catch((error) => {
+                if (error instanceof Error) {
+                    alert(error.message, Severity.ERROR);
+                }
+            });
+    }, [alert, accountsService]);
+
+
+    const onConfirmStockDialog = async (
+        accountId: number,
+        ticker: string,
+        exchange: string,
+        quantity: number,
+        price: number,
+        date: Dayjs,
+    ) => {
+        try {
+            await accountDetailsService.buyStocks(
+                accountId,
+                ticker,
+                exchange,
+                quantity,
+                price,
+                date,
+            );
+        } catch (error) {
+            if (error instanceof Error) {
+                alert(error.message, Severity.ERROR);
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const isDialogOpen = (type: DialogType) => {
+        return dialogOpen && dialogType === type;
+    };
+
+    const openDialog = useCallback((type: DialogType) => {
+        setDialogType(type);
+        setDialogOpen(true);
+    }, []);
+
+    const closeDialog = useCallback(() => {
+        setDialogOpen(false);
+    }, []);
+
+
+    return {
+        accounts,
+        isDialogOpen,
+        closeDialog,
+        openDialog,
+        onConfirmStockDialog,
+    }
+
+}
