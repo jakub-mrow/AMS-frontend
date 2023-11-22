7520544e16c62a68dbdeba7e908c4ac1cd388a39
Dawid Stasiak <dawid.stasiak21@gmail.com>
2023-11-22 08:04:24 +0100

    fix: Redirect after login to initial location

--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,28 +1,27 @@
-import AuthContext from '../auth/auth-context';
-import LoginForm from './LoginForm';
-import LoginPanel from './LoginPanel';
+import AuthContext from "../auth/auth-context";
+import LoginForm from "./LoginForm";
+import LoginPanel from "./LoginPanel";
 import { useContext, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useLogin } from "./use-login.ts";
 
 const Login = () => {
   const { isLoggedIn } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const { changeToRedirectRoute } = useLogin();
   useEffect(() => {
     if (isLoggedIn) {
-      navigate("/");
+      changeToRedirectRoute();
     }
-  }, [isLoggedIn, navigate]);
+  }, [isLoggedIn, changeToRedirectRoute]);
   return (
     <div className="flex w-full h-screen">
       <div className="hidden lg:flex flex-col h-full w-1/2 items-center justify-center bg-primary">
-        <LoginPanel/>
+        <LoginPanel />
       </div>
       <div className="w-full flex flex-col items-center justify-center lg:w-1/2 p-50">
-        <LoginForm/>
+        <LoginForm />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
