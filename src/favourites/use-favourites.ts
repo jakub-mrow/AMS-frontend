a3d8d38f63e4575c597e1b52efc2feceece2ae61
Jakub Mrówczyński <jakub@mrowczynski.net>
2023-12-04 20:06:38 +0100

    fetch data about exchanges from backend

--- src/favourites/use-favourites.ts
+++ src/favourites/use-favourites.ts
@@ -1,12 +1,35 @@
+import useStockExchanges from "./exchanges/use-stock-exchanges";
 import useFavouriteAssets from "./favouriteAssets/use-favourite-assets"
 
 export const useFavourites = () => {
     const { favouriteAssets, deleteFavouriteAsset, viewFavouriteAsset } = useFavouriteAssets();
 
+    const {
+        currentPage,
+        showFavorites,
+        handlePageChange,
+        addToFavorites,
+        isFavorite,
+        toggleShowFavorites,
+        filteredExchanges,
+        totalPages,
+        startIndex,
+        endIndex,
+    } = useStockExchanges();
 
     return {
         favouriteAssets,
         deleteFavouriteAsset,
-        viewFavouriteAsset
+        viewFavouriteAsset,
+        currentPage,
+        showFavorites,
+        handlePageChange,
+        addToFavorites,
+        isFavorite,
+        toggleShowFavorites,
+        filteredExchanges,
+        totalPages,
+        startIndex,
+        endIndex,
     }
 }
